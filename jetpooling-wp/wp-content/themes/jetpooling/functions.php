3e5051ff355055d98f402924ce7ce67185d734a9
unknown <dhruv@leo9studio.com>
2023-08-12 15:29:10 +0530

    changes

--- jetpooling-wp/wp-content/themes/jetpooling/functions.php
+++ jetpooling-wp/wp-content/themes/jetpooling/functions.php
@@ -38,6 +38,7 @@ function blankslate_enqueue()
     // wp_enqueue_style('blankslate-style', get_stylesheet_uri());
     // wp_enqueue_script('jquery');
     wp_enqueue_style('swiper-bundle-css', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.3.2/swiper-bundle.min.css', false, '1.1', 'all');
+    wp_enqueue_style('aos-css', 'https://unpkg.com/aos@2.3.1/dist/aos.css', false, '1.1', 'all');
     wp_enqueue_style('main-css', get_template_directory_uri() . '/assets/css/main.css', false, '1.1', 'all');
 }
 add_action('wp_footer', 'blankslate_footer');
